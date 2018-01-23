c8c16dcbff9b5f9c7b91c6b67e2cbc835f8640d4
Unknown <maciekzaborek@o2.pl>
2018-01-23 23:10:22 +0100

    whatever

--- puzzle.hs
+++ puzzle.hs
@@ -14,12 +14,19 @@ getFileName = do
 {-loadFile = do
     fileName <- getFileName
 -}
-
+{-
 sampleRows = [1, 0, 2, 1, 2, 1]
 sampleColumns = [1, 1, 2, 1, 1, 1]
 sampleHouses = [(0, 1), (3, 2), (3, 4), (4, 0), (4, 4), (5, 2), (5, 5)]--row, col
+-}
+
+sampleRows = [3, 0, 3, 0, 1, 2]
+sampleColumns = [2, 1, 1, 2, 1, 2]
+sampleHouses = [(0,0), (0,4), (1,5), (2,1), (2, 4), (3,0), (3, 3), (4,4), (5,1)]
+
 
 sampleData = initPuzzleData sampleRows sampleColumns sampleHouses
+sampleData2 = placeHouses sampleHouses sampleData
 
 tankData = placeElement (0, 0) 'U' sampleData
 
@@ -31,6 +38,7 @@ H - domek
 X - puste pole
 L - zbiornik przyłączony do domku po lewej
 R, U, D - analogicznie
+T- niepodlaczony zbiornik ??
 -}
 
 
@@ -49,19 +57,117 @@ main =
 findSolution :: PuzzleData -> PuzzleData
 findSolution (PuzzleData rows cols pdata)
     | solutionFound (PuzzleData rows cols pdata) = PuzzleData rows cols pdata
-    | otherwise = findSolution (placeTanks(crossOutRowsAndCols(PuzzleData rows cols pdata)))
+    | otherwise = findSolution (tryEmptyFields(placeTanks(crossOutRowsAndCols(PuzzleData rows cols pdata))))
+--jak brak zmian, to wstaw w puste miejsce, jak się nie udało, to się cofnij i szukaj rozwiązania dalej
+
 
 --sprawdza, czy wszystkie zbiorniki zostaly juz umieszczone
 solutionFound :: PuzzleData -> Bool
 solutionFound (PuzzleData rows cols pdata)
-    | rows == [tanksInRow x (PuzzleData rows cols pdata) | x <- [0..(length rows-1)]]= True
+    | rows == [tanksInRow x (PuzzleData rows cols pdata) | x <- [0..(length rows-1)]]= True--Length od dobrej wartosci?
     | otherwise = False
 
 
---TODO
+
 --przechodzi po macierzy i probuje umiescic zbiorniki
 placeTanks :: PuzzleData -> PuzzleData
-placeTanks (PuzzleData rows cols pdata) = PuzzleData rows cols pdata
+placeTanks (PuzzleData rows cols pdata) -- = PuzzleData rows cols pdata
+    = placeTanks' ((length cols)-1) ((length rows)-1) (PuzzleData rows cols pdata) --Length od dobrej wartosci?
+
+tryEmptyFields :: PuzzleData -> PuzzleData
+tryEmptyFields (PuzzleData rows cols pdata) =
+    tryEmptyFields' ((length cols)-1) ((length rows)-1) (PuzzleData rows cols pdata) --Length od dobrej wartosci?
+
+tryEmptyFields' :: Int -> Int -> PuzzleData -> PuzzleData
+tryEmptyFields' row col (PuzzleData rows cols pdata)
+    | row < 0 = (PuzzleData rows cols pdata)
+    | col < 0 = tryEmptyFields' (row-1) 0 (tryToPlace row 0 (PuzzleData rows cols pdata)) 
+    | otherwise = tryEmptyFields' row (col-1) (tryToPlace row col (PuzzleData rows cols pdata)) 
+
+--jeśli puste pole, nie sąsiaduje ze zbiornikiem, ma domek obok i można wstawić (kolumny i wiersze), to wstawia zbiornik
+tryToPlace :: Int -> Int -> PuzzleData -> PuzzleData
+tryToPlace row col (PuzzleData rows cols pdata)
+    | ((getElement (row, col) (PuzzleData rows cols pdata)) == Just ' ') && noGasAround row col (PuzzleData rows cols pdata) && oneHouseToConnect row col (PuzzleData rows cols pdata) && cols!!col > tanksInCol col (PuzzleData rows cols pdata) && rows!!row > tanksInRow row (PuzzleData rows cols pdata) = (placeTank (row, col) 'D' (PuzzleData rows cols pdata)) 
+    | otherwise = (PuzzleData rows cols pdata)
+
+noGasAround row col pdata
+    =not (isMaybeTank(getElement (row+1, col-1) pdata)) &&  not (isMaybeTank(getElement (row+1, col) pdata)) && not (isMaybeTank(getElement (row+1, col+1) pdata)) && not (isMaybeTank(getElement (row, col-1) pdata)) &&  not (isMaybeTank(getElement (row, col+1) pdata)) && not (isMaybeTank(getElement (row-1, col-1) pdata)) &&  not (isMaybeTank(getElement (row-1, col) pdata)) && not (isMaybeTank(getElement (row-1, col+1) pdata))
+
+oneHouseToConnect :: Int -> Int -> PuzzleData -> Bool    
+oneHouseToConnect row col pdata
+        = (length (filter (== Just 'H')[getElement (row+1, col) pdata, getElement(row-1, col) pdata, getElement(row, col+1) pdata, getElement(row, col-1) pdata])) == 1 && (emptyAround (houseRow, houseCol) pdata) == 1 
+        where
+            houseRow =  if((getElement (row, col+1) pdata) == Just 'H' || (getElement (row, col-1) pdata) == Just 'H')
+                            then row
+                            else if ((getElement (row-1, col) pdata) == Just 'H')
+                                then row-1
+                                else row + 1
+            houseCol =  if((getElement (row+1, col) pdata) == Just 'H' || (getElement (row-1, col) pdata) == Just 'H')
+                            then col
+                            else if ((getElement (row, col-1) pdata) == Just 'H')
+                                then col-1
+                                else col + 1
+
+--housePossibleSpots (houseRow, houseCol) pdata = 
+--    | length (filter (== Just ' ') [getElement position pdata | position <- [(-1, -1), (1, -1), (-1, 1), (1, 1)]])
+
+
+isMaybeTank :: Maybe Char -> Bool
+isMaybeTank elem 
+    | elem == Just 'D' = True
+    | elem == Just 'U' = True
+    | elem == Just 'L' = True
+    | elem == Just 'R' = True
+    |otherwise = False
+
+placeTanks' :: Int-> Int -> PuzzleData -> PuzzleData
+placeTanks' row col (PuzzleData rows cols pdata)
+    | row < 0 = (PuzzleData rows cols pdata)
+    | row < 0 = placeTanks' (row) (col-1) (placeTanksInCol col (PuzzleData rows cols pdata))
+   -- | col < 0 = placeTanks' (row-1) col (placeTanksInRow row (PuzzleData rows cols pdata))
+   -- | otherwise = placeTanks' row (col-1) (placeTanksInCol col (PuzzleData rows cols pdata))
+    |otherwise = placeTanks' (row-1) (col) (placeTanksInRow row (PuzzleData rows cols pdata))
+   -- | otherwise = placeTanks' (row-1) (col) (PuzzleData rows cols pdata)
+
+--jesli tyle samo wolnych miejs, co powinno byc zbiornikow - umiesc zbiornik na kazdym miejscu wolnym
+--jesli sasiaduja one tylko z jednym domkiem - podepnij go do niego, jesli z wiecej niz jednym - podepnij do takiego, ktory moze miec zbiornik tylko w jednym miejscu lub zostaw niepodpiety
+placeTanksInRow :: Int -> PuzzleData -> PuzzleData
+placeTanksInRow row (PuzzleData rows cols pdata)
+    | rows!!(row) == length (V.filter (==' ') (getRow (row+1) pdata)) = placeTanksInRow' 0 row (PuzzleData rows cols pdata)
+    | otherwise = PuzzleData rows cols pdata
+
+--dziala okej
+placeTanksInRow' :: Int -> Int -> PuzzleData -> PuzzleData
+placeTanksInRow' elem row (PuzzleData rows cols pdata)
+--    | rows$row == length (V.filter (==' ') (getRow (row+1) pdata)) =
+    | elem == (length cols) = (PuzzleData rows cols pdata)--Length od dobrej wartosci?
+    | (getElement (row, elem) (PuzzleData rows cols pdata)) == Just ' ' = placeTanksInRow' (elem+1) row (placeTank (row, elem) 'D' (PuzzleData rows cols pdata))
+    | otherwise = placeTanksInRow' (elem+1) row (PuzzleData rows cols pdata)
+
+--jesli domek moze miec zbiornik tylko w jednym miejscu, to go tam postaw
+--TODO
+
+--czy dziala? chyba
+placeTanksInCol :: Int -> PuzzleData -> PuzzleData
+placeTanksInCol col (PuzzleData rows cols pdata)
+    | cols!!(col) == length (V.filter (==' ') (getCol (col+1) pdata)) = placeTanksInCol' 0 col (PuzzleData rows cols pdata)
+    | otherwise = PuzzleData rows cols pdata
+
+--dziala okej
+placeTanksInCol' :: Int -> Int -> PuzzleData -> PuzzleData
+placeTanksInCol' elem col (PuzzleData rows cols pdata)
+--    | rows$row == length (V.filter (==' ') (getRow (row+1) pdata)) =
+    | elem == (length rows) = (PuzzleData rows cols pdata)--Length od dobrej wartosci?
+    | (getElement (elem, col) (PuzzleData rows cols pdata)) == Just ' ' = placeTanksInCol' (elem+1) col (placeTank (elem, col) 'D' (PuzzleData rows cols pdata))
+    | otherwise = placeTanksInCol' (elem+1) col (PuzzleData rows cols pdata)
+
+
+--ile pustych pol jest dookola (po bokach? - jesli 1 i domek nie jest podloczony, to tylko tam moze miec podpiety zbiornik)
+emptyAround :: (Int, Int) -> PuzzleData -> Int
+emptyAround (row, col) pdata =
+    length (filter (== Just ' ') [getElement (row+x, col+y) pdata| x <- [-1, 0, 1], y <- [-1, 0, 1], (x, y) /= (0, 0)])
+--    length (filter (== Just ' ') [getElement position pdata | position <- [(-1, -1), (1, -1), (-1, 1), (1, 1)]])
+
 
 --umieszcza na zadanym wspolrzednych dany zbiornik i wykresla wolne pola dookola
 placeTank :: (Int, Int) -> Char -> PuzzleData -> PuzzleData
@@ -87,7 +193,7 @@ crossOutIfEmpty position pdata
 
 --dla kazdej kolumny/rzedu - jesli liczba zbiornikow jest rowna docelowej liczbie - wykreslone zostaja pozostale puste miejsca
 crossOutRowsAndCols :: PuzzleData -> PuzzleData
-crossOutRowsAndCols (PuzzleData rows cols pdata) = crossOutRowsAndCols' (length rows - 1) (length cols - 1) (PuzzleData rows cols pdata)
+crossOutRowsAndCols (PuzzleData rows cols pdata) = crossOutRowsAndCols' (length rows - 1) (length cols - 1) (PuzzleData rows cols pdata)--Length od dobrej wartosci?
 crossOutRowsAndCols' :: Int -> Int -> PuzzleData -> PuzzleData
 crossOutRowsAndCols' row col (PuzzleData rows cols pdata)
     | row < 0 = (PuzzleData rows cols pdata)
@@ -111,6 +217,7 @@ isTank elem
     | elem == 'L' = True
     | elem == 'R' = True
     |otherwise = False
+
 
 crossOutColumn :: Int -> PuzzleData -> PuzzleData
 crossOutColumn col (PuzzleData rows cols pdata) = crossOutColumn' (length rows -1) col (PuzzleData rows cols pdata)--Length od dobrej wartosci?
@@ -151,12 +258,14 @@ checkCrossOut row col pdata
 
 --wyświetlenie rozwiązania
 displaySolution :: PuzzleData -> IO()
-displaySolution (PuzzleData _ _ pdata) = do putStrLn $ prettyMatrix pdata
+displaySolution (PuzzleData rows cols pdata) = do putStrLn $ prettyMatrix (pdata)
+--displaySolution (PuzzleData rows cols pdata) = do putStrLn $ prettyMatrix (extendTo '0' (1+length rows) (1+length cols) pdata)
+--displaySolution' rows cols extended = displaySolution' ()
 
 --możliwość zapisania rozwiązania do pliku o podanej nazwie
 --saveToFile
 
-data PuzzleData = PuzzleData [Int][Int](Matrix Char)
+data PuzzleData = PuzzleData [Int][Int](Matrix Char) deriving (Eq)
 initPuzzleData :: [Int] -> [Int] -> [(Int, Int)] -> PuzzleData
 initPuzzleData a b h =
     PuzzleData a b (fromList (length b) (length a) [' ', ' '..])--Length od dobrej wartosci?
